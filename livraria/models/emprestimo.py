bcc76250af755393290d7d6a221609f0bf0dfecf
ArianeJunkes <arianejunkes312@gmail.com>
2023-10-25 11:38:27 -0300

    feito alteração model emprestimo

--- livraria/models/emprestimo.py
+++ livraria/models/emprestimo.py
@@ -2,7 +2,9 @@ from django. db import models
 
 class Emprestimo(models.Model):
     nome = models.CharField(max_length=255)
-    dia = models.DateField(max_length=255)
+    contato = models.CharField(max_length=255)
+    inicio = models.DateField(max_length=255)
+    final = models.DateField(max_length=255)
 
     def __str__(self):
         return f"{self.nome}"
